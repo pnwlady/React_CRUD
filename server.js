cc5d0fcb4742f30e5ba91b25e03ddad0d9763d32
heidilaursen <heidimlaursen@gmail.com>
2016-05-16 08:23:51 -0700

    adding characters, actions, routes and model

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ var React = require('react');
 var ReactDOM = require('react-dom/server');
 var Router = require('react-router');
 var routes = require('./app/routes');
+var mongoose = require('mongoose');
+var Character = require('./models/character');
+var config = require('./config');
 
 var app = express();
 
@@ -17,6 +20,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(config.database);
+mongoose.connection.on('error', function() {
+  console.info('Error: Could not connect to MongoDB. Did you forget to run `mongod`?');
+});
 
 app.use(function(req, res) {
   Router.match({ routes: routes.default, location: req.url }, function(err, redirectLocation, renderProps) {
